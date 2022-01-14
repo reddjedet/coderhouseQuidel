06555ea3b589bc4b33b223d8d502167f5f0378b3
Christian Quidel <quidelchristian@gmail.com>
2022-01-14 20:57:12 -0300

    corregido en vivo

--- desafioCoder-simulador/js/main.js
+++ desafioCoder-simulador/js/main.js
@@ -1,17 +1,17 @@
 /* Falta agregar el getElementById vinculado a el boton search*/
 
+let nombre = prompt("Ingresa el nombre del juego: ");
+let entrada = parseFloat(prompt("Ingrese su precio en Steam"));
+
 //Actividad 4
 function calcular(nombre, entrada) {
-  nombre = prompt("Ingresa el nombre del juego: ");
-  entrada = parseFloat(prompt("Ingrese su precio en Steam"));
-
   console.log(
     `${nombre} tiene un precio final de $${
       entrada * 0.65 + entrada
     } pesos argentinos`
   );
 }
-calcular();
+calcular(nombre, entrada);
 /* ----------------------------------------------------------------------- */
 
 //Actividad 5
@@ -22,9 +22,9 @@ const online = {
   disponibilidad: false,
 };
 
-console.log(online[nombre]);
-console.log(online[precio]);
-console.log(online[disponibilidad]);
+console.log(online["nombre"]);
+console.log(online["precio"]);
+console.log(online["disponibilidad"]);
 
 /* //Actividad 6 
 productos fisicos sin vender en el stock de un local comercial*/
@@ -36,26 +36,13 @@ const stock = [
   { id: 25, juego: "vegas pro 18", precio: 18703 },
 ];
 
-/* class ConImpuesto extends stock {
-  constructor(juego, precio) {
-    (this.juego = nombre.toLowerCase()), (this.precio = precio);
-
-    impuestos();
-    {
-      this.precio = this.precio * 0.65 + this.precio;
-    }
-  }
-} */
-
 //Agregar a la lista
-const stock = ["26, juegonuevo, 3667.3"];
-stock.push(`otro elemento`);
-console.log(stock.length);
+stock.push({ id: 26, juego: "juegonuevo", precio: 250 });
 
 //Buscar por id
-const buscar = stock.find((producto) => stock.id === 23);
-console.log(buscar.toString());
+const buscar = stock.find((producto) => producto.id === 23);
+console.log(buscar);
 
 //Buscar productos cuyo precio sea mayor de $5.000
-const mayorQuePrecio = stock.filter((producto) => stock.precio > 5000);
-console.log(mayorQuePrecio.toString());
+const mayorQuePrecio = stock.filter((producto) => producto.precio > 5000);
+console.log(mayorQuePrecio);
