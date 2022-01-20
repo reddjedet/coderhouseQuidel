8c7b283d066062aebdf961dc51b65b38b39479d1
Christian Quidel <quidelchristian@gmail.com>
2022-01-20 11:02:04 -0300

    complementario, ejercicio 6

--- desafioCoder-6/Complementario/main.js
+++ desafioCoder-6/Complementario/main.js
@@ -1,5 +1,5 @@
 /* 
-
+Quidel Christian
 Consigna clase 6, ejercicio complementario: 
 codifica una función cuyas instrucciones permitan ordenar una colección (array).
 Preferentemente, recibir el criterio de ordenamiento por parámetro, y mostrar el resultado del
@@ -12,12 +12,12 @@ const letras = [" a", " b", " c", " d", " e", " f", " g"];
 function concatenar(numeros, letras) {
   return numeros + ", " + letras;
 }
-console.log(concatenar(numeros, letras));
 document.getElementById("titulo").innerHTML = concatenar(numeros, letras);
+console.log(concatenar(numeros, letras));
 
 //Devolver los primeros  4 numeros y ultimas 3 letras
 function slicear(numeros, letras) {
   return numeros.slice(0, 4) + ", " + letras.slice(4, 7);
 }
-console.log(slicear(numeros, letras));
 document.getElementById("subtitulo").innerHTML = slicear(numeros, letras);
+console.log(slicear(numeros, letras));
