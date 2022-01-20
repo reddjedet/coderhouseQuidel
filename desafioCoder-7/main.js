fba17f60a9a04eb13fa00d5f41c7baecb825eb68
Christian Quidel <quidelchristian@gmail.com>
2022-01-20 10:58:26 -0300

    ejercicio complementario, clase 7

---
+++ desafioCoder-7/main.js
@@ -0,0 +1,23 @@
+/* 
+
+Consigna clase 7, ejercicio complementario: 
+codifica una función cuyas instrucciones permitan ordenar una colección (array).
+Preferentemente, recibir el criterio de ordenamiento por parámetro, y mostrar el resultado del
+procesamiento en una salida.*/
+
+const numeros = [1, 2, 3, 4, 5, 6];
+const letras = [" a", " b", " c", " d", " e", " f", " g"];
+
+/* Devolver numeros y letras  concatenados */
+function concatenar(numeros, letras) {
+  return numeros + ", " + letras;
+}
+console.log(concatenar(numeros, letras));
+document.getElementById("titulo").innerHTML = concatenar(numeros, letras);
+
+//Devolver los primeros  4 numeros y ultimas 3 letras
+function slicear(numeros, letras) {
+  return numeros.slice(0, 4) + ", " + letras.slice(4, 7);
+}
+console.log(slicear(numeros, letras));
+document.getElementById("subtitulo").innerHTML = slicear(numeros, letras);
