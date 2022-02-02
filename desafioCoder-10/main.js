9211e2010479f89cca2e735fe5811f969343e915
Christian Quidel <quidelchristian@gmail.com>
2022-02-01 23:14:20 -0300

    Correccion

--- desafioCoder-10/main.js
+++ desafioCoder-10/main.js
@@ -24,11 +24,11 @@ $("h5").append(`
 
 <div class="container">
 
+<div class="nombre-personaje">Nombre:  </div> 
 <textarea id="Input" cols="35" rows="1" placeholder="Inserte un nombre para su personaje"></textarea>
-  <div class="nombre-personaje">Nombre:  <hr></div>
-<p>
-    <button id="append">Aceptar</button>
-</p>
+  <br>
+    <button id="append">Ingresar</button>
+
   
 
     <form action="" id="form-seleccion">
@@ -48,36 +48,38 @@ $("h5").append(`
 
 </div>`);
 
+//Nombre
 $("#append").click(function () {
   $(".nombre-personaje").text(function () {
     return $("#Input").val();
   });
 });
 
+//Selector
 const vistaPrevia = seleccion.addEventListener("change", function () {
   if (this.value == "p1") {
     document.body.style.backgroundImage = "url('./img/gh.png')";
     document.body.style.backgroundRepeat = "no-repeat";
-    $(".nombre-personaje").prepend(` ${descripcionGh}`);
+    $(".nombre-personaje").prepend(` ${descripcionWr}`);
   } else if (this.value == "p2") {
     document.body.style.backgroundImage = "url('./img/wr.png')";
     document.body.style.backgroundRepeat = "no-repeat";
-    $(".nombre-personaje").prepend(` ${descripcionGh}`);
+    $(".nombre-personaje").prepend(` ${descripcionWr}`);
   } else if (this.value == "p3") {
     document.body.style.backgroundImage = "url('./img/adven.png')";
     document.body.style.backgroundRepeat = "no-repeat";
-    $(".nombre-personaje").prepend(` ${descripcionGh}`);
+    $(".nombre-personaje").prepend(` ${descripcionAdv}`);
   } else if (this.value == "p4") {
     document.body.style.backgroundImage = "url('./img/st.png')";
     document.body.style.backgroundRepeat = "no-repeat";
-    $(".nombre-personaje").prepend(` ${descripcionGh}`);
+    $(".nombre-personaje").prepend(` ${descripcionSt}`);
   } else if (this.value == "p5") {
     document.body.style.backgroundImage = "url('./img/et.png')";
     document.body.style.backgroundRepeat = "no-repeat";
-    $(".nombre-personaje").prepend(` ${descripcionGh}`);
+    $(".nombre-personaje").prepend(` ${descripcionEt}`);
   } else if (this.value == "p6") {
     document.body.style.backgroundImage = "url('./img/pk.png')";
     document.body.style.backgroundRepeat = "no-repeat";
-    $(".nombre-personaje").prepend(` ${descripcionGh}`);
+    $(".nombre-personaje").prepend(` ${descripcionPk}`);
   }
 });
