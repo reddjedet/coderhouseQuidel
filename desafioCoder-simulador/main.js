83ebf37203a6bd82772906c4ffc27b9543692190
Christian Quidel <quidelchristian@gmail.com>
2022-02-17 22:10:18 -0300

    segundo reinicio

---
+++ desafioCoder-simulador/main.js
@@ -0,0 +1,218 @@
+/* 
+
+$(".container-gh").hide();
+$(".container-wr").hide();
+$(".container-adven").hide();
+$(".container-st").hide();
+$(".container-et").hide();
+$(".container-pk").hide();
+$(".container-gs").hide();
+$(".container-ms").hide();
+$(".container-sag").hide();
+$(".container-ss").hide();
+$(".container-mm").hide();
+$(".container-so").hide(); */
+
+/* $(".container-gh").hide();
+$(".container-wr").hide();
+$(".container-adven").hide();
+$(".container-st").hide();
+$(".container-et").hide();
+$(".container-pk").hide();
+$(".container-gs").hide();
+$(".container-ms").hide();
+$(".container-sag").hide();
+$(".container-ss").hide();
+$(".container-mm").hide();
+$(".container-so").hide();  */
+
+/* $("#btn-gh").click(function () {
+  $(".container-gh").slideToggle(350);
+}); */
+
+//Botones reveal
+$("#btn-gh").click(() => {
+  $(".container-gh").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-gh").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-wr").click(() => {
+  $(".container-wr").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-wr").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-adven").click(() => {
+  $(".container-adven").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-adven").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-st").click(() => {
+  $(".container-st").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-st").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-et").click(() => {
+  $(".container-et").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-et").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-pk").click(() => {
+  $(".container-pk").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-pk").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-gs").click(() => {
+  $(".container-gs").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-gs").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-ms").click(() => {
+  $(".container-ms").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-ms").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-sag").click(() => {
+  $(".container-sag").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-sag").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-ss").click(() => {
+  $(".container-ss").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-ss").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-mm").click(() => {
+  $(".container-mm").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-mm").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-so").click(() => {
+  $(".container-so").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-so").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+//agregar iteraciones
+//agregar objetos
+//agregar json
+//agregar arrays
+//agregar dom
