5c0d260c262490e5167147fd13761f69c73d6088
Christian Quidel <quidelchristian@gmail.com>
2022-02-19 12:32:17 -0300

    actualizando lista de html

--- desafioCoder-simulador/main.js
+++ desafioCoder-simulador/main.js
@@ -1,3 +1,10 @@
+/* const clickBotonLlevaEmpiezaTaller = function() => {
+
+
+
+}
+ */
+
 //Botones reveal
 $("#btn-gh").click(() => {
   $(".container-gh").animate(
@@ -179,42 +186,158 @@ $("#btn-so").click(() => {
   );
 });
 
+$("#btn-arh").click(() => {
+  $(".container-arch").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-arch").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-duel").click(() => {
+  $(".container-duel").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-duel").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-dread").click(() => {
+  $(".container-dread").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-dread").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-gk").click(() => {
+  $(".container-gk").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-gk").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-ti").click(() => {
+  $(".container-ti").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-ti").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-fs").click(() => {
+  $(".container-fs").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-fs").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-maes").click(() => {
+  $(".container-maes").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-maes").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-sm").click(() => {
+  $(".container-sm").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-sm").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-em").click(() => {
+  $(".container-em").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-em").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
+$("#btn-al").click(() => {
+  $(".container-al").animate(
+    {
+      height: "toggle",
+      opacity: 0.9,
+      marginTop: 7,
+    },
+    function () {
+      $(".container-al").animate({
+        marginLeft: 85,
+      });
+    }
+  );
+});
+
 //agregar iteraciones
 //agregar objetos
 //agregar json
 //agregar arrays
 //agregar dom
-
-/* 
-
-for boton-personaje of boton-personajes
-if boton-seleccionado =  personaje 1
-show descripcion
-show dyes
-
-show pregunta: que te gustaria cambiar
-show resultado
-
-
-*/
-
-/* var img = $('<img />', { 
-  id: 'Myid',
-  src: 'MySrc.gif',
-  alt: 'MyAlt'
-});
-img.appendTo($('#YourDiv')); */
-
-/* function imagenPersonajeNuevo(raza, tipo, sexo, imagen) {
-  this.raza = raza;
-  this.tipo = tipo;
-  this.sexo = sexo;
-  this.imagen = ;
-}
-
-
-
-const taller = document.querySelector("#taller");
-taller.innerHTML = 
-$("#btn-seleccion").;
- */
