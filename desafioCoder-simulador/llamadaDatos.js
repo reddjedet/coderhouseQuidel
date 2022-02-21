35285e06de542a9df4a10ebd1b241b4ee4a60b1f
Christian Quidel <quidelchristian@gmail.com>
2022-02-21 18:36:01 -0300

    primeras pruebas con fetch

--- desafioCoder-simulador/llamadaDatos.js
+++ desafioCoder-simulador/llamadaDatos.js
@@ -1,20 +1,6 @@
 /* ---------------------- */
-
 const URLGET = "./clasesPersonajes.json";
-$("#btn-seleccion").click(() => {
-  //el click debiera cambiarle el id segun cual boton sea
 
-  $.get(URLGET, function (respuesta, estado) {
-    if (estado === "success") {
-      let misDatos = respuesta;
-      for (const dato of misDatos) {
-        $(".container-elegido").append(`<h3>
-        ${raza.value}
-        ${tipo.value}
-        ${stats.value}
-        </h3>
-        `);
-      }
-    }
-  });
-});
+fetch(URLGET)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
